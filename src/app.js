7cf0adc40ce1cf76550a4919468ddd1483c208c2
zhukunpenglinyutong <270750933@qq.com>
2019-07-09 14:54:38 +0800

    第5次提交（14:54）| 增加记录日志的功能

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
+const path = require('path');
+const fs = require('fs')
 const Koa = require('koa');
 const app = new Koa();
-const routing = require('./routes')
-const error = require('koa-json-error')
-const parameter = require('koa-parameter')
-const bodyBody = require('koa-body')
-const config = require('./config/index')
+const error = require('koa-json-error') // 错误处理插件
+const parameter = require('koa-parameter') // 解析URL参数
+const bodyBody = require('koa-body') // 解析请求体
+const logger= require('koa-morgan') // 日志插件
+const routing = require('./routes') // 路由文件
+const config = require('./config/index') // 配置
 
 app.use(error()) // 错误处理返回
 app.use(bodyBody()) // 解析 请求body中的 JSON
 app.use(parameter(app)) // 参数校验，加入app的话，就能在全局使用了
 
+
+// 日志（开发环境，打印在console.log）
+if (process.env.NODE_DEV === 'development') app.use(logger('dev'));
+// 日志（上线环境，打包在logs目录下）
+if (process.env.NODE_DEV === 'production') {
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' })
+    app.use(logger('combined', { stream: accessLogStream }))
+}
 
 // 连接数据库
 const mongoose = require('mongoose')
